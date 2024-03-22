b88b19bd44f4a409a24ae5508bfaed1ce453d27b
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-03-22 13:40:34 -0400

    Ecriture des methodes (vides) dans le controleur

--- ProjetPompier_AppWeb/Controllers/CaserneController.cs
+++ ProjetPompier_AppWeb/Controllers/CaserneController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetPompier_API.Logics.Models;
 
 namespace ProjetPompier_AppWeb.Controllers
 {
@@ -7,7 +8,42 @@ namespace ProjetPompier_AppWeb.Controllers
         [Route("")]
         [Route("Caserne")]
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [Route("Caserne/AjouterCaserne")]
+        [HttpGet]
+        public async Task<IActionResult> AjouterCaserne(CaserneDTO caserneDTO)
+        {
+            return View();
+        }
+
+        [Route("Caserne/FormulaireModificationCaserne")]
+        [HttpGet]
+        public async Task<IActionResult> FormulaireModificationCaserne(string nomCaserne)
+        {
+            return View();
+        }
+
+        [Route("Caserne/ModifierCaserne")]
+        [HttpGet]
+        public async Task<IActionResult> ModifierCaserne(CaserneDTO caserneDTO)
+        {
+            return View();
+        }
+
+        [Route("Caserne/SupprimerCaserne")]
+        [HttpGet]
+        public async Task<IActionResult> SupprimerCaserne(string nomCaserne)
+        {
+            return View();
+        }
+
+        [Route("Caserne/ViderListeCaserne")]
+        [HttpGet]
+        public async Task<IActionResult> ViderListeCaserne()
         {
             return View();
         }
