042e2360279627f78a5389c373ca9357e5c5d888
LyesHamrani <124377714+LyesHamrani@users.noreply.github.com>
2024-04-19 13:16:59 -0400

    Maj

--- ProjetPompier_AppWeb/Controllers/PompierController.cs
+++ ProjetPompier_AppWeb/Controllers/PompierController.cs
@@ -8,30 +8,48 @@ namespace ProjetPompier_AppWeb.Controllers
 {
     public class PompierController : Controller
     {
+        /// <summary>
+        /// Méthode de service appelé lors de l'action Index.
+        /// </summary>
+        /// <param name="nomCaserne">Le nom de la caserne</param>
+        /// <param name="seulementCapitaine">Filtre de l'affichage</param>
+        /// <returns>Retourne l'index</returns>
         [Route("Pompier")]
         [HttpGet]
         public async Task<IActionResult> Index([FromQuery] string nomCaserne, [FromQuery] string seulementCapitaine)
         {
-            JsonValue jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Caserne/ObtenirListeCaserne");
-            List<CaserneDTO> listeCaserneDTO = JsonConvert.DeserializeObject<List<CaserneDTO>>(jsonResponse.ToString());
-            ViewBag.ListeCaserne = listeCaserneDTO;
+            try
+            { 
+                // Appeler le service web pour obtenir la liste des casernes
+                JsonValue jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Caserne/ObtenirListeCaserne");
+                List<CaserneDTO> listeCaserneDTO = JsonConvert.DeserializeObject<List<CaserneDTO>>(jsonResponse.ToString());
+                ViewBag.ListeCaserne = listeCaserneDTO;
 
-            if (string.IsNullOrEmpty(nomCaserne) && string.IsNullOrEmpty(seulementCapitaine))
-            {
-                nomCaserne = listeCaserneDTO.FirstOrDefault()?.Nom; // Utiliser le premier nom de caserne si aucun n'est spécifié
-                seulementCapitaine = "false"; // Utiliser la valeur par défaut si rien n'est spécifié
+                if (string.IsNullOrEmpty(nomCaserne) && string.IsNullOrEmpty(seulementCapitaine))
+                {
+                    nomCaserne = listeCaserneDTO.FirstOrDefault()?.Nom; // Utiliser le premier nom de caserne si aucun n'est spécifié
+                    seulementCapitaine = "false"; // Utiliser la valeur par défaut si rien n'est spécifié
+                }
+
+                // Appeler le service web pour obtenir la liste des pompiers
+                jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Pompier/ObtenirListePompier?nomCaserne=" + nomCaserne + "&seulementCapitaine=" + seulementCapitaine);
+                List<PompierDTO> listePompierDTO = JsonConvert.DeserializeObject<List<PompierDTO>>(jsonResponse.ToString());
+                ViewBag.ListePompier = listePompierDTO;
+                ViewBag.NomCaserne = nomCaserne;
+                ViewBag.SeulementCapitaine = seulementCapitaine;
+
+                // Appeler le service web pour obtenir la liste des grades
+                jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Grade/ObtenirListeGrade");
+                List<GradeDTO> listeGradeDTO = JsonConvert.DeserializeObject<List<GradeDTO>>(jsonResponse.ToString());
+                ViewBag.ListeGrade = listeGradeDTO;
             }
 
-            jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Pompier/ObtenirListePompier?nomCaserne=" + nomCaserne + "&seulementCapitaine=" + seulementCapitaine);
-            List<PompierDTO> listePompierDTO = JsonConvert.DeserializeObject<List<PompierDTO>>(jsonResponse.ToString());
-            ViewBag.ListePompier = listePompierDTO;
-            ViewBag.NomCaserne = nomCaserne;
-            ViewBag.SeulementCapitaine = seulementCapitaine;
-
-            jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Grade/ObtenirListeGrade");
-            List<GradeDTO> listeGradeDTO = JsonConvert.DeserializeObject<List<GradeDTO>>(jsonResponse.ToString());
-            ViewBag.ListeGrade = listeGradeDTO;
-
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+                //Mettre cette ligne en commentaire avant de lancer les tests fonctionnels
+                TempData["MessageErreur"] = e.Message;
+            }
 
             return View(); // Retourner la vue
         }
