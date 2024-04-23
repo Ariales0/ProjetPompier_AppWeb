3831373ad4d1ed0f65d9beafd6fa1d1ce93844d6
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-04-23 09:19:24 -0400

    Ajout des DTO crée par Vincent VehiculeDTO et TypeVehiculeDTO

---
+++ ProjetPompier_AppWeb/Logics/Models/TypeVehiculeDTO.cs
@@ -0,0 +1,42 @@
+﻿
+/// <summary>
+/// Namespace pour les classe de type DTOs.
+/// </summary>
+namespace AppMobile_ProjetPompierC.DTO
+{
+    /// <summary>
+    /// Classe représentant le DTO d'un type de véhicule.
+    /// </summary>
+    public class TypeVehiculeDTO
+    {
+        #region Proprietes
+
+        /// <summary>
+        /// Propriété représentant le type du véhicule.
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// Propriété représentant le code du véhicule.
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Propriété représentant le nombre de personne du véhicule.
+        /// </summary>
+        public int NombrePersonne { get; set; }
+
+       /// <summary>
+       /// Propriété représentant le nombre de personne du véhicule.
+       /// </summary>
+       /// <param name="type"></param>
+       /// <param name="code"></param>
+       /// <param name="nombrePersonne"></param>
+        public TypeVehiculeDTO( string type = "", string code, int nombrePersonne)
+        {
+            Type = type;
+            Code = code;
+            NombrePersonne = nombrePersonne;
+        }
+
+        #endregion Constructeurs
+    }
+}
---
+++ ProjetPompier_AppWeb/Logics/Models/VehiculeDTO.cs
@@ -0,0 +1,75 @@
+﻿
+/// <summary>
+/// Namespace pour les classe de type DTOs.
+/// </summary>
+namespace AppMobile_ProjetPompierC.DTO
+{
+    /// <summary>
+    /// Classe représentant le DTO d'un véhicule.
+    /// </summary>
+    public class VehiculeDTO
+    {
+        #region Proprietes
+        /// <summary>
+        /// Propriété représentant le VIN du véhicule.
+        /// </summary>
+        public string VinVehicule { get; set; }
+        /// <summary>
+        /// Propriété représentant le code du véhicule.
+        /// </summary>
+        public int Code { get; set; }
+        /// <summary>
+        /// Propriété représentant le type du véhicule.
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// Propriété représentant la marque du véhicule.
+        /// </summary>
+        public string Marque { get; set; }
+        /// <summary>
+        /// Propriété représentant le modèle du véhicule.
+        /// </summary>
+        public string Modele { get; set; }
+        /// <summary>
+        /// Propriété représentant l'année du véhicule.
+        /// </summary>
+        public int Annee { get; set; }
+        /// <summary>
+        /// Propriété représentant le nombre de personne du véhicule.
+        /// </summary>
+        public int NombrePersonne { get; set; }
+
+
+        #endregion Proprietes
+
+        #region Constructeurs
+        /// <summary>
+        /// Constructeur vide.
+        /// </summary>
+        public VehiculeDTO() { }
+
+        /// <summary>
+        /// Constructeur avec paramètres.
+        /// </summary>
+        /// <param name="vinVehicule"></param>
+        /// <param name="code"></param>
+        /// <param name="type"></param>
+        /// <param name="marque"></param>
+        /// <param name="modele"></param>
+        /// <param name="annee"></param>
+        /// <param name="nombrePersonne"></param>
+        public VehiculeDTO(string vinVehicule, int code, string type, string marque, string modele, int annee, int nombrePersonne)
+        {
+			VinVehicule = vinVehicule;
+            Code = code;
+            Type = type;
+            Marque = marque;
+            Modele = modele;
+            Annee = annee;
+            NombrePersonne = nombrePersonne;
+            
+        }
+
+        #endregion Constructeurs
+    }
+}
