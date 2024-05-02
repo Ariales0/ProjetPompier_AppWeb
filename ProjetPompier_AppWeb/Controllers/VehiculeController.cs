1377a158cc1c83af0172ffc0ce809673f8a44f33
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-05-02 08:35:37 -0400

    vEHICULE OK

---
+++ ProjetPompier_AppWeb/Controllers/VehiculeController.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjetPompier_AppWeb.Logics.Models;
+using ProjetPompier_AppWeb.Utils;
+using System.Json;
+
+namespace ProjetPompier_AppWeb.Controllers
+{
+    public class VehiculeController : Controller
+    {
+        /// <summary>
+        /// Méthode de service appelé lors de l'action Index.
+        /// </summary>
+        /// <param name="nomCaserne">Le nom de la caserne</param>
+        /// <returns>Retourne l'index</returns>
+        [Route("Vehicule")]
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] string nomCaserne)
+        {
+            try
+            {
+                // Appeler le service web pour obtenir la liste des casernes
+                JsonValue jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Caserne/ObtenirListeCaserne");
+                List<CaserneDTO> listeCaserneDTO = JsonConvert.DeserializeObject<List<CaserneDTO>>(jsonResponse.ToString());
+                ViewBag.ListeCaserne = listeCaserneDTO;
+
+                if (string.IsNullOrEmpty(nomCaserne))
+                {
+                    nomCaserne = listeCaserneDTO.FirstOrDefault()?.Nom; // Utiliser le premier nom de caserne si aucun n'est spécifié
+                }
+
+                // Appeler le service web pour obtenir la liste des Vehicules
+                jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Vehicule/ObtenirListeVehicule?nomCaserne=" + nomCaserne);
+                List<VehiculeDTO> listeVehiculeDTO = JsonConvert.DeserializeObject<List<VehiculeDTO>>(jsonResponse.ToString());
+                ViewBag.ListeVehicule = listeVehiculeDTO;
+                ViewBag.NomCaserne = nomCaserne;
+
+                // Appeler le service web pour obtenir la liste des type de véhicule
+                jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/TypesVehicule/ObtenirListeTypesVehicule");
+                List<TypeVehiculeDTO> listeTypeVehiculeDTO = JsonConvert.DeserializeObject<List<TypeVehiculeDTO>>(jsonResponse.ToString());
+                ViewBag.ListeTypeVehicule = listeTypeVehiculeDTO;
+            }
+
+            catch (Exception e)
+            {
+                ViewBag.MessageErreurCritique = e.Message;
+                //Mettre cette ligne en commentaire avant de lancer les tests fonctionnels
+                TempData["MessageErreur"] = e.Message;
+            }
+
+            return View(); // Retourner la vue
+        }
+
+        /// <summary>
+        /// Méthode de service appelé lors de l'action AjouterVehicule.
+        /// </summary>
+        /// <param name="nomCaserne">Le nom de la caserne</param>
+        /// <param name="vehicule">Le DTO du vehicule à ajouter</param>
+        /// <returns>Retourne l'index vehicule</returns>
+        [Route("Vehicule/AjouterVehicule")]
+        [HttpPost]
+        public async Task<IActionResult> AjouterVehicule(string nomCaserne, VehiculeDTO vehicule)
+        {
+            // Appeler le service web pour ajouter un vehicule
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Vehicule/AjouterVehicule?nomCaserne=" + nomCaserne, vehicule);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreurCritique = e.Message;
+                //Mettre cette ligne en commentaire avant de lancer les tests fonctionnels
+                TempData["MessageErreur"] = e.Message;
+            }
+            // Rediriger vers l'index vehicule
+            return RedirectToAction("Index", "Vehicule", new { nomCaserne });
+        }
+
+        /// <summary>
+        /// Méthode de service appelé lors de l'action FormulaireModifierVehicule.
+        /// </summary>
+        /// <param name="nomCaserne">Le nom de la caserne</param>
+        /// <param name="vinVehicule">Le vin du véhicule à modifier</param>
+        /// <returns>Retourne le formulaire modifier vehicule</returns>
+        [Route("Vehicule/FormulaireModifierVehicule")]
+        [HttpGet]
+        public async Task<IActionResult> FormulaireModifierVehicule([FromQuery] string nomCaserne, [FromQuery] string vinVehicule)
+        {
+            try
+            {
+                // Appeler le service web pour obtenir un véhicule
+                JsonValue jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Vehicule/ObtenirVehicule?nomCaserne=" + nomCaserne + "&vinVehicule=" + vinVehicule);
+                VehiculeDTO vehiculeDTO = JsonConvert.DeserializeObject<VehiculeDTO>(jsonResponse.ToString());
+                // Appeler le service web pour obtenir la liste des type de véhicule
+                jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/TypesVehicule/ObtenirListeTypesVehicule");
+                List<TypeVehiculeDTO> listeTypeVehiculeDTO = JsonConvert.DeserializeObject<List<TypeVehiculeDTO>>(jsonResponse.ToString());
+                ViewBag.ListeTypeVehicule = listeTypeVehiculeDTO;
+                ViewBag.NomCaserne = nomCaserne;
+                return View(vehiculeDTO);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+                return RedirectToAction("Index", "Vehicule", new { nomCaserne });
+            }
+        }
+
+        /// <summary>
+        ///  Méthode de service appelé lors de l'action Modifiervehicule.
+        /// </summary>
+        /// <param name="nomCaserne">Nom de la caserne</param>
+        /// <param name="vehicule">Object véhicule modifié</param>
+        /// <returns>Retourne la page vehicule</returns>
+        [Route("Vehicule/ModifierVehicule")]
+        [HttpPost]
+        public async Task<IActionResult> ModifierVehicule(string nomCaserne, VehiculeDTO vehicule)
+        {
+            // Appeler le service web pour modifier un vehicule
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Vehicule/ModifierVehicule?nomCaserne=" + nomCaserne, vehicule);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreurCritique = e.Message;
+                //Mettre cette ligne en commentaire avant de lancer les tests fonctionnels
+                TempData["MessageErreur"] = e.Message;
+            }
+            // Rediriger vers l'index vehicule
+            return RedirectToAction("Index", "Vehicule", new { nomCaserne });
+        }
+
+
+        /// <summary>
+        ///  Méthode de service appelé lors de l'action SupprimmerVehicule.
+        /// </summary>
+        /// <param name="nomCaserne">Nom de la caserne</param>
+        /// <param name="vinVehicule">Matricule du vehicule</param>
+        /// <returns>Retourne la page vehicule</returns>
+        [Route("Vehicule/SupprimerVehicule")]
+        [HttpPost]
+        public async Task<IActionResult> SupprimmerVehicule(string nomCaserne, string vinVehicule)
+        {
+            // Appeler le service web pour supprimer un vehicule
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Vehicule/SupprimerVehicule?nomCaserne=" + nomCaserne + "&vinVehicule=" + vinVehicule, null);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreurCritique = e.Message;
+                //Mettre cette ligne en commentaire avant de lancer les tests fonctionnels
+                TempData["MessageErreur"] = e.Message;
+            }
+            // Rediriger vers l'index vehicule
+            return RedirectToAction("Index", "Vehicule", new {  nomCaserne });
+        }
+
+        [Route("Vehicule/ViderListeVehicule")]
+        [HttpPost]
+        public async Task<IActionResult> ViderListeVehicule(string nomCaserne)
+        {
+            // Appeler le service web pour vider la liste des vehicule
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Vehicule/ViderListeVehicules?nomCaserne=" + nomCaserne, null);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreurCritique = e.Message;
+                //Mettre cette ligne en commentaire avant de lancer les tests fonctionnels
+                TempData["MessageErreur"] = e.Message;
+            }
+            // Rediriger vers l'index vehicule
+            return RedirectToAction("Index", "Vehicule", new { nomCaserne });
+        }
+
+    }
+}
--- ProjetPompier_AppWeb/Logics/Models/VehiculeDTO.cs
+++ ProjetPompier_AppWeb/Logics/Models/VehiculeDTO.cs
@@ -1,75 +1,68 @@
 ﻿
+
 /// <summary>
 /// Namespace pour les classe de type DTOs.
 /// </summary>
-namespace ProjetPompier_AppWeb.Logics.Models
+namespace ProjetPompier_AppWeb.Logics.Models;
+
+/// <summary>
+/// Classe représentant le DTO d'un véhicule.
+/// </summary>
+public class VehiculeDTO
 {
+    #region Proprietes
     /// <summary>
-    /// Classe représentant le DTO d'un véhicule.
+    /// Propriété représentant le VIN du véhicule.
     /// </summary>
-    public class VehiculeDTO
+    public string Vin { get; set; }
+
+    /// <summary>
+    /// Propriété représentant le code du véhicule.
+    /// </summary>
+    public int Code { get; set; }
+    /// <summary>
+    /// Propriété représentant la marque du véhicule.
+    /// </summary>
+    public string Marque { get; set; }
+    /// <summary>
+    /// Propriété représentant le modèle du véhicule.
+    /// </summary>
+    public string Modele { get; set; }
+    /// <summary>
+    /// Propriété représentant l'année du véhicule.
+    /// </summary>
+    public int Annee { get; set; }
+
+
+
+    #endregion Proprietes
+
+    #region Constructeurs
+    /// <summary>
+    /// Constructeur vide.
+    /// </summary>
+    public VehiculeDTO()
     {
-        #region Proprietes
-        /// <summary>
-        /// Propriété représentant le VIN du véhicule.
-        /// </summary>
-        public string VinVehicule { get; set; }
-        /// <summary>
-        /// Propriété représentant le code du véhicule.
-        /// </summary>
-        public int Code { get; set; }
-        /// <summary>
-        /// Propriété représentant le type du véhicule.
-        /// </summary>
-        public string Type { get; set; }
-        /// <summary>
-        /// Propriété représentant la marque du véhicule.
-        /// </summary>
-        public string Marque { get; set; }
-        /// <summary>
-        /// Propriété représentant le modèle du véhicule.
-        /// </summary>
-        public string Modele { get; set; }
-        /// <summary>
-        /// Propriété représentant l'année du véhicule.
-        /// </summary>
-        public int Annee { get; set; }
-        /// <summary>
-        /// Propriété représentant le nombre de personne du véhicule.
-        /// </summary>
-        public int NombrePersonne { get; set; }
-
-
-        #endregion Proprietes
-
-        #region Constructeurs
-        /// <summary>
-        /// Constructeur vide.
-        /// </summary>
-        public VehiculeDTO() { }
-
-        /// <summary>
-        /// Constructeur avec paramètres.
-        /// </summary>
-        /// <param name="vinVehicule"></param>
-        /// <param name="code"></param>
-        /// <param name="type"></param>
-        /// <param name="marque"></param>
-        /// <param name="modele"></param>
-        /// <param name="annee"></param>
-        /// <param name="nombrePersonne"></param>
-        public VehiculeDTO(string vinVehicule, int code, string type, string marque, string modele, int annee, int nombrePersonne)
-        {
-			VinVehicule = vinVehicule;
-            Code = code;
-            Type = type;
-            Marque = marque;
-            Modele = modele;
-            Annee = annee;
-            NombrePersonne = nombrePersonne;
-            
-        }
-
-        #endregion Constructeurs
     }
+
+    /// <summary>
+    /// Constructeur avec paramètres.
+    /// </summary>
+    /// <param name="vin">Le vin</param>
+    /// <param name="typeVehicule">Le type de vehicule</param>
+    /// <param name="marque">La marque</param>
+    /// <param name="modele">Le modele</param>
+    /// <param name="annee">L'annee</param>
+    public VehiculeDTO(string vin = "", int code = 0000, string marque = "", string modele = "", int annee = 0000)
+    {
+        Vin = vin;
+        Code = code;
+        Marque = marque;
+        Modele = modele;
+        Annee = annee;
+    }
+
+
+
+    #endregion Constructeurs
 }
