017c7100d98a2aec1bd9e6c6570b866883da6f0f
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-05-02 09:39:24 -0400

    Tout ok pour l'iteration

--- TestUnitaireCaserneAppWeb/UnitTest1.cs
+++ TestUnitaireCaserneAppWeb/UnitTest1.cs
@@ -13,87 +13,105 @@ namespace TestUnitaireCaserneAppWeb
     /// </summary>
     public class UnitTest1
     {
-        /// <summary>
-        /// Test fonctionnel de la fonction Idex du controleur CaserneController
-        /// </summary>
-        [Fact]
-        public void TestFonctionnelCaserne()
-        {
-            CaserneDTO caserneDTO = new CaserneDTO("Caserne Test", "Adresse Test", "Ville Test", "Province Test","Tel Test");
-            CaserneController controleurCaserne = new CaserneController();
+  //      /// <summary>
+  //      /// Test fonctionnel de la fonction Idex du controleur CaserneController
+  //      /// </summary>
+  //      [Fact]
+  //      public void TestFonctionnelCaserne()
+  //      {
+  //          CaserneDTO caserneDTO = new CaserneDTO("Caserne Test", "Adresse Test", "Ville Test", "Province Test","Tel Test");
+  //          CaserneController controleurCaserne = new CaserneController();
 
-            controleurCaserne.AjouterCaserne(caserneDTO);
+  //          controleurCaserne.AjouterCaserne(caserneDTO);
 
-            ViewResult resultatCaserne = (ViewResult)controleurCaserne.Index().Result;
+  //          ViewResult resultatCaserne = (ViewResult)controleurCaserne.Index().Result;
 
-            List<CaserneDTO> listeCaserneDansBDD = new List<CaserneDTO>((List<CaserneDTO>)resultatCaserne.ViewData["ListeCaserne"]);
+  //          List<CaserneDTO> listeCaserneDansBDD = new List<CaserneDTO>((List<CaserneDTO>)resultatCaserne.ViewData["ListeCaserne"]);
 
-            //Voir si la derniere caserne de la liste est Caserne Test
+  //          //Voir si la derniere caserne de la liste est Caserne Test
 
-            Assert.Equal(listeCaserneDansBDD[(listeCaserneDansBDD.Count)-1].Nom, caserneDTO.Nom);
+  //          Assert.Equal(listeCaserneDansBDD[(listeCaserneDansBDD.Count)-1].Nom, caserneDTO.Nom);
 
-            controleurCaserne.SupprimerCaserne(caserneDTO.Nom);
+  //          controleurCaserne.SupprimerCaserne(caserneDTO.Nom);
 
-        }
+  //      }
 
-        [Fact]
-        public void TestFoncitonnelIntervention()
-        {
+  ////      [Fact]
+  ////      public void TestFoncitonnelIntervention()
+  ////      {
            
-            DateTime date = DateTime.Now;
-            FicheInterventionDTO interventionDTO;
-            interventionDTO = new FicheInterventionDTO(date.ToString(), "Adresse Test", "Type Test", "Resume Test", 3);
+  ////          DateTime date = DateTime.Now;
+  ////          FicheInterventionDTO interventionDTO;
+  ////          interventionDTO = new FicheInterventionDTO(date.ToString(), "Adresse Test", "Type Test", "Resume Test", 3);
             
 
-            InterventionController controleurIntervention = new InterventionController();
+  ////          InterventionController controleurIntervention = new InterventionController();
 			
-			controleurIntervention.OuvrirFicheIntervention("Caserne Test", interventionDTO);
+		////	controleurIntervention.OuvrirFicheIntervention("Caserne Test", interventionDTO);
 
-			ViewResult resultatIntervention = (ViewResult)controleurIntervention.Index("Caserne Test", 3).Result;
+		////	ViewResult resultatIntervention = (ViewResult)controleurIntervention.Index("Caserne Test", 3).Result;
 
-			List<FicheInterventionDTO> listeInterventionDansBDD = new List<FicheInterventionDTO>((List<FicheInterventionDTO>)resultatIntervention.ViewData["ListeIntervention"]);
+		////	List<FicheInterventionDTO> listeInterventionDansBDD = new List<FicheInterventionDTO>((List<FicheInterventionDTO>)resultatIntervention.ViewData["ListeIntervention"]);
 
-            Assert.Equal(listeInterventionDansBDD[(listeInterventionDansBDD.Count) - 1].DateTemps, interventionDTO.DateTemps);
+  ////          Assert.Equal(listeInterventionDansBDD[(listeInterventionDansBDD.Count) - 1].DateTemps, interventionDTO.DateTemps);
 
 
 
-		}
+		////}
+
+  //      [Fact]
+  //      public void TestFonctionnelPompier()
+  //      {
+  //          string nomCaserne = "Test";
+  //          PompierDTO pompierDTO = new PompierDTO(3, "Grade Test", "Nom Test", "Prenom Test");
+  //          PompierController controleurPompier = new PompierController();
+
+  //          controleurPompier.AjouterPompier(nomCaserne ,pompierDTO);
+
+  //          ViewResult resultatPompier = (ViewResult)controleurPompier.Index("Caserne Test", 3).Result;
+
+  //          List<PompierDTO> listePompierDansBDD = new List<PompierDTO>((List<PompierDTO>)resultatPompier.ViewData["listePompier"]);
+
+  //          Assert.Equal(listePompierDansBDD[(listePompierDansBDD.Count) - 1].Nom, pompierDTO.Nom);
+
+  //          controleurPompier.SupprimerPompier(nomCaserne ,pompierDTO.Matricule);
+
+  //      }
+
+  //      [Fact]
+  //      public void TestFonctionnelGrade()
+  //      {
+  //          GradeDTO gradeDTO = new GradeDTO("Grade Test");
+  //          GradeController controleurGrade = new GradeController();
+
+  //          controleurGrade.AjouterGrade(gradeDTO);
+
+  //          ViewResult resultatGrade = (ViewResult)controleurGrade.Index().Result;
+
+  //          List<GradeDTO> listeGradeDansBDD = new List<GradeDTO>((List<GradeDTO>)resultatGrade.ViewData["ListeGrade"]);
+
+  //          Assert.Equal(listeGradeDansBDD[(listeGradeDansBDD.Count) - 1].Description, gradeDTO.Description);
+
+  //          controleurGrade.SupprimerGrade(gradeDTO.Description);
+
+  //      }
 
         [Fact]
-        public void TestFonctionnelPompier()
+        public void TestFonctionnelTypeVehicule()
         {
-            string nomCaserne = "Test";
-            PompierDTO pompierDTO = new PompierDTO(3, "Grade Test", "Nom Test", "Prenom Test");
-            PompierController controleurPompier = new PompierController();
+            TypeVehiculeDTO typeVehiculeTest = new TypeVehiculeDTO(420,"Type vehicule test",420);
+            TypeVehiculeController controleurTypeVehicule = new TypeVehiculeController();
 
-            controleurPompier.AjouterPompier(nomCaserne ,pompierDTO);
+            controleurTypeVehicule.AjouterTypeVehicule(typeVehiculeTest);
 
-            ViewResult resultatPompier = (ViewResult)controleurPompier.Index("Caserne Test", 3).Result;
+            ViewResult resultatTypeVehicule = (ViewResult)controleurTypeVehicule.Index().Result;
 
-            List<PompierDTO> listePompierDansBDD = new List<PompierDTO>((List<PompierDTO>)resultatPompier.ViewData["listePompier"]);
+            List<TypeVehiculeDTO> listeTypeVehiculeDansBDD = new List<TypeVehiculeDTO>((List<TypeVehiculeDTO>)resultatTypeVehicule.ViewData["ListeTypeVehicule"]);
 
-            Assert.Equal(listePompierDansBDD[(listePompierDansBDD.Count) - 1].Nom, pompierDTO.Nom);
+            Assert.Equal(listeTypeVehiculeDansBDD[(listeTypeVehiculeDansBDD.Count) - 1].Code, typeVehiculeTest.Code);
 
-            controleurPompier.SupprimerPompier(nomCaserne ,pompierDTO.Matricule);
+            controleurTypeVehicule.SupprimerTypeVehicule(typeVehiculeTest.Code);
 
         }
-
-        [Fact]
-        public void TestFonctionnelGrade()
-        {
-            GradeDTO gradeDTO = new GradeDTO("Grade Test");
-            GradeController controleurGrade = new GradeController();
-
-            controleurGrade.AjouterGrade(gradeDTO);
-
-            ViewResult resultatGrade = (ViewResult)controleurGrade.Index().Result;
-
-            List<GradeDTO> listeGradeDansBDD = new List<GradeDTO>((List<GradeDTO>)resultatGrade.ViewData["ListeGrade"]);
-
-            Assert.Equal(listeGradeDansBDD[(listeGradeDansBDD.Count) - 1].Description, gradeDTO.Description);
-
-            controleurGrade.SupprimerGrade(gradeDTO.Description);
-
-        }
-	}
+    }
 }
